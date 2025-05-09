0caa68ad7c736a287e826849ed382fc3f9b10558
Adya Kumari <adyajha776@gmail.com>
2025-05-09 11:06:37 +0530

    Card Like aadhar card only using HTML,CSS,JS

---
+++ card.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const card = document.querySelector(".card");
+  
+    card.addEventListener("click", () => {
+      card.classList.toggle("flipped");
+    });
+  });
+  
