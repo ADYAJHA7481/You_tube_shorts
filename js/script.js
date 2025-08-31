f3ded50492a602274fc9a8bafeceade3fec74590
Adya Kumari <adyajha776@gmail.com>
2025-08-31 13:41:11 +0530

    Add some js mini project

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// let FullName = prompt("Enter your fullname : ");
+// console.log(`@${FullName.trim().replaceAll(" ","").toLowerCase()}_${FullName.length}`);
+
+// let str = prompt("Enter any String :");
+// let ch = prompt("Enter any character :");
+// let count = 0;
+// for(let val of str){
+//     if(val.toLowerCase()===ch.toLowerCase()){
+//         count++;
+//     }
+// }
+// console.log(`your string is ${str}`);
+// console.log(`character length is ${count}`);
+
+
+let str = "hello aknna nin nsn dnai dwdn jid kdm jdj jd jdjj";
+console.log(str.split(" ").length);
